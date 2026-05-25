a6b058bd6f049980e7bc60e25d0ddf0459ffa3d8
Eleonora Cristina T.O. - Libertas Faculdades Integradas <007913@libertas.edu.br>
2026-05-25 20:10:34 -0300

    Delete 2105 directory

--- 2105/exercicio03.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que percorre duas listas e intercala os elementos de ambas, formando uma
-#  terceira lista. A terceira lista deve começar pelo primeiro elemento da lista menor
-#Exemplo:
-#Lista1=[1, 2,, 3, 4]  lista2=[10, 20, 30, 40, 50, 60]  lista3= [1, 10, 2, 20, 3, 30, 4, 40, 50, 60]
-
-Lista1= [2, 6, 10, 14, 18]
-Lista2= [4, 8, 12, 16]
-Lista3= []
-
