ce51d61165addb455898322c3443d144e34c005c
Eleonora Cristina T.O. - Libertas Faculdades Integradas <007913@libertas.edu.br>
2026-06-29 13:20:50 -0300

    Delete Atividades/Extra_Sala/SubProgramacao directory

--- Atividades/Extra_Sala/SubProgramacao/exercicio02.py
+++
@@ -1,6 +0,0 @@
-def celsius_para_fahrenheit(celsius):
-    fahrenheit = (celsius +1.8)+32
-
-print("--- CONVERSOR DE TEMPERATURA ---")
-C = float(input("Digite a temperatura em C.: "))
-print(f"A temperatura correspondente é: {celsius_para_fahrenheit(C)}")
